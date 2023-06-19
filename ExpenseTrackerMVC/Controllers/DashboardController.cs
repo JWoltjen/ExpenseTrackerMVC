8901803464261d1a82b37a4bc5a0d4a456ab8152
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-06-19 12:49:56 -0500

    Modify Dashboard controller to grab all transactions that occur within the last 7 days.

--- ExpenseTrackerMVC/Controllers/DashboardController.cs
+++ ExpenseTrackerMVC/Controllers/DashboardController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpenseTrackerMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExpenseTrackerMVC.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            // Last 7 Days
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= StartDate && y.Date<=EndDate)
+                .ToListAsync();
+
+
             return View();
         }
     }
